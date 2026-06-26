7fd6c445e0b542e3d3eae1b378465920f92a3552
Stelios Mavris <smavris1@gmail.com>
2026-06-26 11:45:16 +0300

    feat: create project's initial skeleton and create entities

---
+++ src/main/java/org/example/ainewssummarizer/AiNewsSummarizerApplication.java
@@ -0,0 +1,13 @@
+package org.example.ainewssummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNewsSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNewsSummarizerApplication.class, args);
+	}
+
+}
